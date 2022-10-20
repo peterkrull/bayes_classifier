98cb51c1b1b89be95dbb4709d118a78857ff9bda
Peter Krull <peterkrullpeter@gmail.com>
2022-10-20 22:44:02 +0200

    Removed single threaded function,updated python lib accordingly

--- python/naive_bayes.py
+++ python/naive_bayes.py
@@ -6,7 +6,7 @@ import sys
 # sys.path.append("/home/peterkrull/multivariate_gauss_pdf/target/release")
 import naive_bayes_module as bayesian
 
-def classifier(x:pd.DataFrame,m:list[np.array],s:list[np.array],target:str = None,multi=True):
+def classifier(x:pd.DataFrame,m:list[np.array],s:list[np.array],target:str = None):
     """Naive Bayesian classification of entire data sets. Built in rust for fearless concurrency.
 
     Args:
@@ -14,7 +14,6 @@ def classifier(x:pd.DataFrame,m:list[np.array],s:list[np.array],target:str = Non
         `m (list[np.array])`: Python list with class means as numpy arrays
         `s (list[np.array])`: Python list with class covariances as numpy arrays
         `target (str, optional)`: Key of column containing class labels for all samples. Defaults to None.
-        `multi (bool, optional)`: Wether to use multi or single threaded rust function. Defaults to True.
 
     Returns:
         `np.array`: array of estimated classes
@@ -30,10 +29,7 @@ def classifier(x:pd.DataFrame,m:list[np.array],s:list[np.array],target:str = Non
     M = np.ascontiguousarray(m)
     S = np.ascontiguousarray(s)
            
-    if multi:
-        est = bayesian.classifier_multi(X,M,S)
-    else:
-        est = bayesian.classifier_single(X,M,S)
+    est = bayesian.classifier_multi(X,M,S)
         
     if target:
         tru = x.get('target')
