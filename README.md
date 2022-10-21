ae6562e2870828a38eeec0350a1875c09813a3a4
Peter Krull <peterkrullpeter@gmail.com>
2022-10-21 20:28:18 +0200

    Simplified Python interface

--- README.md
+++ README.md
@@ -7,41 +7,17 @@ Simple Python-module built in Rust to classify large data sets concurrently in s
 The most basic use of the classifier involved fitting a gaussian distribution to each class based on a labeled data set.
 
 ``` python
-from rust_bayes import classifier_class
+from rust_bayes import classifier
 
 X = # DataFrame containing data to classify 
 y = # DataFrame containing data labels 
 
 # Get classifier object
-bayes = rust_bayes.classifier_class()
+bayes = classifier()
 
 # Fit the model to a data set
 bayes.fit(X,y)
 
 # Predict 
 est = bayes.predict(X_test)
-```
-
-Alternatively, given a list of means and a list of covariances, a set of samples can be evaluated as such:
-
-```python
-import rust_bayes
-
-X = # DataFrame containing data to classify 
-M = # List of class means as numpy.array
-S = # List of class covariances as numpy.array
-P = # Class priors as a numpy.array
-
-est = rust_bayes.classifier(X,M,S,P)
-
-# If priors are not available, uniform priors are assumed:
-est = rust_bayes.classifier(X,M,S)
-```
-It is also possible to provide a string to the classification function, signifying the name of the column containing the labels of each sample in `X`. This causes the function to also return a confusion matrix, such that the performance of the classification can be evaluated easily.
-
-``` python
-est, conf = rust_bayes.classifier(X,M,S,P,'target')
-
-# Omitting the priors argument results in the priors being determined from target column of X
-est, conf = rust_bayes.classifier(X,M,S,target='target')
 ```
--- python/rust_bayes.py
+++ python/rust_bayes.py
@@ -1,78 +1,47 @@
 import pandas as pd
 import numpy as np
 import time
-import sys
 
-# sys.path.append("/home/peterkrull/multivariate_gauss_pdf/target/release")
 import rust_bayes_module as bayes
 
-def classifier(x:pd.DataFrame,m:list[np.array],s:list[np.array],p:list[float] = None,target:str = None,uniform_priors=False):
-    """Bayesian classification of entire data sets. Built in rust for fearless concurrency.
-
-    Args:
-        `x (pd.DataFrame)`: Pandas dataframe containing the samples (row-wise) to classify.
-        `m (list[np.array])`: Python list with class means as numpy arrays
-        `s (list[np.array])`: Python list with class covariances as numpy arrays
-        `p (list[float], optional)`: Python list with priors, can be determined form data if `target` is supplied
-        `target (str, optional)`: Key of column containing class labels for all samples. Defaults to None.
-
-    Returns:
-        `np.array`: array of estimated classes
-        or, if target is supplied
-        `np.array` , `np.array`: array of estimated classes and confusion matrix
-    """
-    start = time.time()
-    if target:
-        X = np.ascontiguousarray(x.drop(target,axis=1).to_numpy())
-    else:
-        X = np.ascontiguousarray(x.to_numpy())
-        
-    M = np.ascontiguousarray(m)
-    S = np.ascontiguousarray(s)
-    P = np.ascontiguousarray(p)
-    
-    # Determine priors
-    if type(p).__module__ == np.__name__:
-        P = np.ascontiguousarray(p,dtype = float)
-    elif target and not uniform_priors:
-        classes = np.sort(x['target'].unique())
-        P = np.ascontiguousarray([len(x[x[target] == c]) for c in classes],dtype = float)
-        print(P)
-        print(f"Warning : No priors were supplied, assuming priors from target column :\n{P/len(x['target'])}")
-    else:
-        if uniform_priors:
-            print("Using uniform priors")
-        else:
-            print("Warning : No priors were supplied, assuming uniform priors.")
-        P = np.ascontiguousarray([1.0]*S.shape[0],dtype = float)
-           
-    est = bayes.classifier_multi(X,M,S,P)
-        
-    if target:
-        
-        # Calculate confusion matrix
-        tru = x.get('target')
-        conf_matrix = np.zeros((len(tru.unique()),len(tru.unique())), dtype=int)
-        np.add.at(conf_matrix, (tru.to_numpy(dtype = int), est), 1)
-        
-        print(f"Classification took : {round(time.time()-start,3)} seconds")
-        return est,conf_matrix
-    else:
-        print(f"Classification took : {round(time.time()-start,3)} seconds")
-        return est
-    
-
 # Basic interface
-class classifier_class:
+class classifier:
 
-    def fit(self,X:pd.DataFrame, y:pd.DataFrame):
+    def fit(self, X:pd.DataFrame, y:pd.DataFrame, p:pd.DataFrame = None, c:pd.DataFrame = None):
+        """Determines the means, covariances and priors for the provided data set
+
+        Args:
+            `X (pd.DataFrame)`: Data set, samples as rows
+            `y (pd.DataFrame)`: Labels for data set
+            `p (pd.DataFrame, optional)`: Priors Defaults to None.
+            `c (pd.DataFrame, optional)`: Subset of classes to use. Defaults to None.
+        """
         
-        classes = np.sort(y.unique())
+        # Get sorted list of all classses in data set        
+        classes = np.sort(y.unstack().unique()) if c == None else c
+    
+        # Calculate mean, covariance and priors of data set
+        self.M = np.ascontiguousarray([X[y[0] == c].mean() for c in classes], dtype = float)
+        self.S = np.ascontiguousarray([X[y[0] == c].cov() for c in classes], dtype = float)
+        self.P = np.ascontiguousarray([len(y[y[0] == c]) for c in classes], dtype = float) if p == None else p
         
-        self.M = [X[y == c].mean() for c in classes]
-        self.S = [X[y == c].cov() for c in classes]
-        self.P = np.ascontiguousarray([len(y[y == c]) for c in classes])
+    def predict(self,X:pd.DataFrame, verbose = True):
+        """Predict the class of the given sample(s)
+
+        Args:
+            `X (pd.DataFrame)`: Data set to classify
+            `verbose (bool)`: Print message with classification time. Defaults to True
+
+        Returns:
+            `(np.array)`: _description_
+        """
         
-    def predict(self,X:pd.DataFrame):
-        return classifier(X,self.M,self.S,self.P)
+        start_time = time.time()
         
+        prediction = bayes.classifier_multi( np.ascontiguousarray(X), self.M, self.S, self.P )
+        
+        if verbose:
+            print(f"Classification of {len(X)} samples took : { round(time.time()-start_time,3) } seconds")
+        
+        return prediction
+    
